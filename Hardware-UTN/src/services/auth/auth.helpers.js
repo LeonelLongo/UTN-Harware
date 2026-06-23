2fce9d79d1d3b4a0de5a877ff634a6c4a7c36bbe
Tobias Auzmendi <auzmendito@gmail.com>
2026-06-22 22:51:45 -0300

    Arreglo error en carpeta auth del Front

---
+++ Hardware-UTN/src/services/auth/auth.helpers.js
@@ -0,0 +1,42 @@
+import { jwtDecode } from "jwt-decode";
+
+export const validateString = (str, minLength, maxLength) => {
+  if (!str) return false;
+  if (minLength && str.length < minLength) return false;
+  else if (maxLength && str.length > maxLength) return false;
+
+  return true;
+};
+
+export const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const validatePassword = (
+  password,
+  minLength,
+  maxLength,
+  needsUppercase,
+  needsNumber,
+) => {
+  if (!password) return false;
+  if (minLength && password.length < minLength) return false;
+  else if (maxLength && password.length > maxLength) return false;
+  else if (needsUppercase && !/[A-Z]/.test(password)) return false;
+  else if (needsNumber && !/\d/.test(password)) return false;
+
+  return true;
+};
+
+export const isTokenValid = (token) => {
+  if (!token) return false;
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return currentTime < decodedToken.exp;
+  } catch (error) {
+    console.error("Error decoding token:", error);
+    return false;
+  }
+};
